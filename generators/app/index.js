ed10734b86aff4231ba0909725de0611def462bc
Pablo Rodriguez <pablo.rodriguez@janeirodigital.com>
2019-01-14 16:49:58 -0600

    added some helper libraries to the generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,94 @@
-'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const figlet = require('figlet');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const glob = require('glob');
+const voca = require('voca');
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the kickass ${chalk.red('generator-solid-react')} generator!`)
-    );
+    const done = this.async();
+    this.log(yosay(chalk.cyan.bold('Welcome to the \n Solid React Generator')));
 
-    const prompts = [
+    return this.prompt([
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: 'input',
+        name: 'appName',
+        message: 'Please enter your application name :',
+        store: true,
+        validate: name => {
+          const pass = name.match(/^[^\d\s!@£$%^&*()+=]+$/);
+          if (pass) {
+            return true;
+          }
+          return `${chalk.red('Provide a valid "App name", digits and whitespaces not allowed')}`;
+        },
+        default: voca.kebabCase(this.appname) // Default to current folder name
       }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+    ]).then(answers => {
+      this.props = answers;
+      done();
     });
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+
+    this.log(chalk.blue(JSON.stringify(this.props.appName)));
+    this.log(chalk.cyan(JSON.stringify(this.destinationPath())));
+    this.log(chalk.cyan(JSON.stringify(this.templatePath("./*"))));
+
+
+    const pkgJson = {}
+
+    this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+    const fromTemplateFiles = glob.sync(this.templatePath('./*'), {
+      ignore: ['**/node_modules', '**/dist'],
+      dot: true
+    });
+    this.log('Copying app directory...');
+    if (path.basename(this.destinationPath()) !== this.props.name) {
+      this.log('Creating folder...');
+      mkdirp(this.props.name);
+      this.destinationRoot(this.destinationPath(this.props.name));
+    }
+
+    this.log(this.templatePath());
+    this.log(this.destinationPath());
+
+    this.fs.copyTpl(
+      fromTemplateFiles,
+      this.destinationRoot(),
+      {
+        name: this.props.name
+      }
     );
   }
 
   install() {
-    this.installDependencies();
+    this.log('Installing dependencies...');
+    this.npmInstall();
+    this.completed = true;
+  }
+
+  end() {
+    if (this.completed) {
+      this.log('Installation complete. Welcome to Solid');
+      this.log(
+        chalk.bold.blue(
+          figlet.textSync('- Solid -', {
+            font: '3D-ASCII',
+            horizontalLayout: 'full',
+            verticalLayout: 'full'
+          })
+        )
+      );
+      return;
+    }
   }
 };
