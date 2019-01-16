70bf2bf3a878a0cbebe9e60ef4a44b3d6b5c3797
Jose Rodriguez <jose.rdrz.rojas@gmail.com>
2019-01-16 10:11:25 -0600

    Templating solid specific assets (favicon, app title)

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,11 +1,11 @@
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const figlet = require('figlet');
-const path = require('path');
-const mkdirp = require('mkdirp');
-const glob = require('glob');
-const voca = require('voca');
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const figlet = require("figlet");
+const path = require("path");
+const mkdirp = require("mkdirp");
+const glob = require("glob");
+const voca = require("voca");
 
 module.exports = class extends Generator {
   constructor(args, opts) {
@@ -14,22 +14,45 @@ module.exports = class extends Generator {
 
   prompting() {
     const done = this.async();
-    this.log(yosay(chalk.cyan.bold('Welcome to the \n Solid React Generator')));
+    this.log(chalk.cyan.bold("Welcome to the \n Solid React Generator"));
 
     return this.prompt([
       {
-        type: 'input',
-        name: 'appName',
-        message: 'Please enter your application name :',
+        type: "input",
+        name: "appName",
+        message: "Please enter your application name :",
         store: true,
-        validate: name => {
-          const pass = name.match(/^[^\d\s!@£$%^&*()+=]+$/);
+        validate: appName => {
+          const pass = appName.match(/^[^\d\s!@£$%^&*()+=]+$/);
           if (pass) {
             return true;
           }
-          return `${chalk.red('Provide a valid "App name", digits and whitespaces not allowed')}`;
+          return `${chalk.red(
+            'Provide a valid "App name", digits and whitespaces not allowed'
+          )}`;
         },
         default: voca.kebabCase(this.appname) // Default to current folder name
+      },
+      {
+        type: "input",
+        name: "appVersion",
+        message: "version:",
+        store: true,
+        validate: appVersion => {
+          const pass = appVersion.match(/^\d{1,2}\.\d{1,2}\.\d{1,2}$/);
+          if (pass) {
+            return true;
+          }
+          return `${chalk.red("Provide a valid version (ex: 0.1.0)")}`;
+        },
+        default: "0.1.0"
+      },
+      {
+        type: "list",
+        name: "isPrivate",
+        message: "Is it private?",
+        choices: ["false", "true"],
+        default: "false"
       }
     ]).then(answers => {
       this.props = answers;
@@ -38,53 +61,49 @@ module.exports = class extends Generator {
   }
 
   writing() {
+    const { appName, appVersion, isPrivate } = this.props;
+    const pkgJson = {
+      name: appName,
+      version: appVersion,
+      private: isPrivate === "true"
+    };
 
-    this.log(chalk.blue(JSON.stringify(this.props.appName)));
-    this.log(chalk.cyan(JSON.stringify(this.destinationPath())));
-    this.log(chalk.cyan(JSON.stringify(this.templatePath("./*"))));
-
-
-    const pkgJson = {}
-
-    this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
-    const fromTemplateFiles = glob.sync(this.templatePath('./*'), {
-      ignore: ['**/node_modules', '**/dist'],
+    this.log(chalk.blue(this.templatePath("package.json")));
+    const fromTemplateFiles = glob.sync(this.templatePath("./*"), {
+      ignore: ["**/node_modules", "**/dist"],
       dot: true
     });
-    this.log('Copying app directory...');
-    if (path.basename(this.destinationPath()) !== this.props.name) {
-      this.log('Creating folder...');
-      mkdirp(this.props.name);
-      this.destinationRoot(this.destinationPath(this.props.name));
+    
+    if (path.basename(this.destinationPath()) !== appName) {
+      this.log("Creating folder...");
+      mkdirp(appName);
+      this.destinationRoot(this.destinationPath(appName));
     }
 
-    this.log(this.templatePath());
-    this.log(this.destinationPath());
+    this.log("Copying app directory...");
 
-    this.fs.copyTpl(
-      fromTemplateFiles,
-      this.destinationRoot(),
-      {
-        name: this.props.name
-      }
-    );
+    this.fs.copyTpl(fromTemplateFiles, this.destinationRoot(), {
+      title: voca.titleCase(appName)
+    });
+    this.fs.extendJSON(this.destinationPath("package.json"), pkgJson);
+
   }
 
   install() {
-    this.log('Installing dependencies...');
+    this.log("Installing dependencies...");
     this.npmInstall();
     this.completed = true;
   }
 
   end() {
     if (this.completed) {
-      this.log('Installation complete. Welcome to Solid');
+      this.log("Installation complete. Welcome to Solid");
       this.log(
         chalk.bold.blue(
-          figlet.textSync('- Solid -', {
-            font: '3D-ASCII',
-            horizontalLayout: 'full',
-            verticalLayout: 'full'
+          figlet.textSync("SOLID", {
+            font: "3D-ASCII",
+            horizontalLayout: "full",
+            verticalLayout: "full"
           })
         )
       );
