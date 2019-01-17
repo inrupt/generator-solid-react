ec70b5d25bfca85842935034e533df79e53ff909
James Martin <james.martin@janeirodigital.com>
2019-01-17 15:02:21 -0500

    Fixing tests and adding a new 404 page test

--- generators/app/templates/src/containers/PageNotFound/page-not-found.test.js
+++ generators/app/templates/src/containers/PageNotFound/page-not-found.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import PageNotFound from './page-not-found.component';
-
-import './utils/enzymeSetup';
+import { Link } from 'react-router-dom';
+import '../../utils/enzymeSetup';
 
 it('App renders without crashing', () => {
   const wrapper = shallow(<PageNotFound />);
   expect(wrapper).toBeTruthy();
+});
+
+it('includes link to homepage', () => {
+  const home = shallow(<PageNotFound/>);
+  expect(home.find(Link)).toHaveLength(1);
 });
