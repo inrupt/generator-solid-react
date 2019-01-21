7f930af69203a975fc956b9a8d2628f8b67baeca
Jose Rodriguez <jose.rdrz.rojas@gmail.com>
2019-01-21 12:52:03 -0600

    removed unnused variable

--- generators/app/templates/src/containers/Register/children/RegistrationSuccess/registration-success.component.js
+++ generators/app/templates/src/containers/Register/children/RegistrationSuccess/registration-success.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { GradientBackground, CenterContainer } from "@util-components";
+import { CenterContainer } from "@util-components";
 import { NavBar } from "@components";
 import { RegistrationPage } from "./registration-success.style";
 
