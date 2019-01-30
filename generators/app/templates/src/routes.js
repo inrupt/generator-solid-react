156e43b6d85cf5021323e12341ebedc2cfedc880
Pablo Rodriguez <pablo.rodriguez@janeirodigital.com>
2019-01-30 13:00:16 -0600

    Profile added to navigation

--- generators/app/templates/src/routes.js
+++ generators/app/templates/src/routes.js
@@ -15,6 +15,11 @@ const privateRoutes = [
     id: "welcome",
     path: "/welcome",
     component: WelcomeComponent
+  },
+  {
+    id: "profile",
+    path: "/profile",
+    component: WelcomeComponent
   }
 ];
 
