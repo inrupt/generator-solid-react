4bbc873afb0e292045a627a327afbacd0c8dc352
Pablo Rodriguez <pablo.rodriguez@janeirodigital.com>
2019-02-08 10:48:39 -0600

    Adding empty solid loading animation

--- generators/app/templates/src/components/AuthNavBar/children/nav-bar-profile.component.js
+++ generators/app/templates/src/components/AuthNavBar/children/nav-bar-profile.component.js
@@ -5,19 +5,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import auth from "solid-auth-client";
 
+
 export const ImageContainer = styled.div`
   width: 42px;
   height: 42px;
   border-radius: 50%;
   background-size: cover;
   overflow: hidden;
-  display: ${({ show }) => (show ? "block" : "none")};
-  img{
-    box-sizing: border-box;
-    width: 100%;
-    height: 100%;
-  }
+  visibility: ${({ show }) => (show ? "visible" : "hidden")};
+  display: ${({ show }) => (show ? "block" : "none")};;`;
+
+export const Img = styled.img`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
 `;
+
+export const LoadingImage = styled(ImageContainer)`
+  background: #cccccc;
+  display:block;
+`;
+
 
 class NavBarProfile extends Component {
   state = {
@@ -56,12 +64,13 @@ class NavBarProfile extends Component {
     return img ? (
       <Dropdown actions={profileOpts} className="nav-bar--profile" hover={true}>
       <ImageContainer show={imageLoaded}>
-        <img 
+        <Img 
+          show={imageLoaded}
           src={img}
           alt="profile"
           onLoad={this.onImageLoaded}/>
       </ImageContainer>
-        {!imageLoaded && <FontAwesomeIcon icon="spinner" spin size="2x" />}
+        {!imageLoaded && <LoadingImage show={true}/>}
       </Dropdown>
     ) : (
       <div />
