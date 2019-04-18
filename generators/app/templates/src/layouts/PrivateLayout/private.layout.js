bccdb764d94388e90c8e6325f069ba4568a60aeb
James Martin <james.martin@janeirodigital.com>
2019-04-17 20:42:27 -0400

    Fixing missing tag

--- generators/app/templates/src/layouts/PrivateLayout/private.layout.js
+++ generators/app/templates/src/layouts/PrivateLayout/private.layout.js
@@ -29,7 +29,7 @@ const PrivateLayout = ({ routes, ...rest }) => {
           <LiveUpdate>
             <AuthNavBar {...matchProps} />
           </LiveUpdate>
-          <Fragment>
+          <Content>
             <Switch>
               {routes.map(route => (
                 <Route key={route.id} {...route} exact />
