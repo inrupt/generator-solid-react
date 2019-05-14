49a23b3512c96fffe376202a1ea5f5bc068cd491
Pablo Rodriguez <pablo.rodriguez@janeirodigital.com>
2019-05-14 13:56:21 -0600

    styles with styled-components

--- generators/app/templates/src/containers/Profile/profile.container.js
+++ generators/app/templates/src/containers/Profile/profile.container.js
@@ -1,16 +1,18 @@
 import React, { Fragment, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'react-i18next';
 
 import { withToastManager } from 'react-toast-notifications';
 import {
-    LiveUpdate,
     useWebId,
     ShexFormBuilder,
 } from '@inrupt/solid-react-components';
-import { Header, ProfileContainer, ProfileWrapper } from './profile.style';
-import { Image, Form } from './components';
-import './profile-shex-form.css';
+import {
+    Header,
+    ProfileContainer,
+    ProfileWrapper,
+    ShexForm,
+} from './profile.style';
+import { Image } from './components';
 
 const defaultProfilePhoto = '/img/icon/empty-profile.svg';
 
@@ -52,28 +54,30 @@ const Profile = ({ toastManager }) => {
                                 }}
                             />
                         </Header>
-                        <ShexFormBuilder
-                            {...{
-                                documentUri: webId,
-                                shexUri:
-                                    'https://shexshapes.inrupt.net/public/userprofile.shex',
-                                theme: {
-                                    form: 'shexForm',
-                                    shexPanel: 'shexPanel',
-                                    shexRoot: 'shexRoot',
-                                    deleteButton:
-                                        'deleteButton ids-button-stroke ids-button-stroke--secondary',
-                                    inputContainer: 'inputContainer',
-                                    addButtonStyle:
-                                        'addButton ids-button-stroke ids-button-stroke--secondary',
-                                },
-                                languageTheme: {
-                                    language: i18n.language.substring(0, 2),
-                                },
-                                successCallback,
-                                errorCallback,
-                            }}
-                        />
+                        <ShexForm>
+                            <ShexFormBuilder
+                                {...{
+                                    documentUri: webId,
+                                    shexUri:
+                                        'https://shexshapes.inrupt.net/public/userprofile.shex',
+                                    theme: {
+                                        form: 'shexForm',
+                                        shexPanel: 'shexPanel',
+                                        shexRoot: 'shexRoot',
+                                        deleteButton:
+                                            'deleteButton ids-button-stroke ids-button-stroke--secondary',
+                                        inputContainer: 'inputContainer',
+                                        addButtonStyle:
+                                            'addButton ids-button-stroke ids-button-stroke--secondary',
+                                    },
+                                    languageTheme: {
+                                        language: i18n.language.substring(0, 2),
+                                    },
+                                    successCallback,
+                                    errorCallback,
+                                }}
+                            />
+                        </ShexForm>
                     </Fragment>
                 )}
             </ProfileContainer>
