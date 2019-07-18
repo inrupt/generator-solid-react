dfbfb05da048bcfd20c1aa071e35751a07d99fe6
Jairo Campos <jairo.campos@janeirodigital.com>
2019-07-18 15:15:28 -0600

    fix eslint warning

--- generators/app/templates/src/containers/TicTacToe/GamePage/children/Board/board.test.js
+++ generators/app/templates/src/containers/TicTacToe/GamePage/children/Board/board.test.js
@@ -3,10 +3,9 @@ import ReactModal from 'react-modal';
 import { render, cleanup } from 'react-testing-library';
 import Board from './board.component';
 
-
 describe.only('TicTacToe', () => {
   afterAll(cleanup);
-  document.querySelectorAll = () => ["node"];
+  document.querySelectorAll = () => ['node'];
   ReactModal.setAppElement(document.createElement('div'));
   const { container } = render(<Board squares={[]} />);
 
--- generators/app/templates/src/containers/TicTacToe/GamePage/children/Game/game.test.js
+++ generators/app/templates/src/containers/TicTacToe/GamePage/children/Game/game.test.js
@@ -4,7 +4,7 @@ import Game from './game.component';
 
 describe.only('TicTacToe', () => {
   afterAll(cleanup);
-  document.querySelectorAll = () => ["node"];
+  document.querySelectorAll = () => ['node'];
 
   const { container } = render(<Game gameData={{}} />);
 
