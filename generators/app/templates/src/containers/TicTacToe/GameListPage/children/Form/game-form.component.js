8c0f6aa48aba3a14b19947f9ebb82a4a9660e5ac
Pablo Rodriguez <pablo.rodriguez@janeirodigital.com>
2019-07-24 12:00:38 -0600

    ACL library integrated with game

--- generators/app/templates/src/containers/TicTacToe/GameListPage/children/Form/game-form.component.js
+++ generators/app/templates/src/containers/TicTacToe/GameListPage/children/Form/game-form.component.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import moment from 'moment';
 import { namedNode } from '@rdfjs/data-model';
-import N3 from 'n3';
+import { AccessControlList } from '@inrupt/solid-react-components';
 import tictactoeShape from '@contexts/tictactoe-shape.json';
 import {
   ldflexHelper,
@@ -114,105 +114,6 @@ const GameForm = ({ webId, sendNotification, opponent, setOpponent }: Props) => 
   };
 
   /**
-   * Creates a file based on a turtle looking string and a url
-   * @param {String} documentUri Game's document url
-   * @param {Object} body Turtle string for the new file
-   */
-  const createACLFile = async (documentUri, body) => {
-    try {
-      ldflexHelper.createDocumentWithTurtle(documentUri, body);
-    } catch (e) {
-      throw new Error('Error while creating ACL');
-    }
-  };
-
-  /**
-   * Creates a quad
-   * @param {String} subject Subject of the quad
-   * @param {String} predicate Predicate of the quad
-   * @param {String} object Object of the quad
-   */
-  const createQuad = (subject, predicate, object) => ({
-    subject,
-    predicate,
-    object
-  });
-
-  /**
-   * Creates all the default quads for a basic ACL turtle
-   * @param {Array} modes Array of modes to create for the ACL
-   * @param {String} webId WebId of the Pod the acl will be assigned to
-   * @param {String} subject Name of the subject (ReadWriteControl)
-   * @param {Array<String>} prefixes Array of prefixes
-   */
-  const createDefaultQuads = (modes, webId, subject, prefixes) => {
-    const { acl, foaf, a } = prefixes;
-    const originalPredicates = [
-      createQuad(subject, `${a}`, namedNode(`${acl}Authorization`)),
-      createQuad(subject, `${acl}accessTo`, namedNode(documentUri))
-    ];
-    const predicates = webId
-      ? [...originalPredicates, createQuad(subject, `${acl}agent`, namedNode(webId))]
-      : [...originalPredicates, createQuad(subject, `${acl}agentClass`, namedNode(`${foaf}Agent`))];
-
-    const quadArray = modes.reduce(
-      (array, mode) => [...array, createQuad(subject, `${acl}mode`, namedNode(`${acl}${mode}`))],
-      predicates
-    );
-
-    return quadArray;
-  };
-
-  /**
-   * Creates the turtle string for the acl file
-   * @param {String} documentUri Document Url
-   * @param {String} opponent Opponent's webId
-   */
-  const aclTurtle = async (documentUri, opponent) => {
-    try {
-      const aclDocument = `${documentUri}.acl`;
-      const { DataFactory } = N3;
-      const { namedNode, quad } = DataFactory;
-      const prefixes = {
-        acl: 'http://www.w3.org/ns/auth/acl#',
-        foaf: 'http://xmlns.com/foaf/0.1/',
-        n: 'http://www.w3.org/2006/vcard/ns#',
-        '': `${aclDocument}#`,
-        a: 'http://www.w3.org/ns/auth/acl#type'
-      };
-      const owner = `${aclDocument}#owner`;
-      const publicSubject = `${aclDocument}#public`;
-      const opponentSubject = `${aclDocument}#opponent`;
-
-      const writer = new N3.Writer({ prefixes });
-      const ownerQuads = createDefaultQuads(['Control', 'Read', 'Write'], webId, owner, prefixes);
-
-      const opponentQuads = createDefaultQuads(
-        ['Read', 'Write'],
-        opponent,
-        opponentSubject,
-        prefixes
-      );
-
-      const publicQuads = createDefaultQuads(['Read'], null, publicSubject, prefixes);
-
-      const quads = [...ownerQuads, ...opponentQuads, ...publicQuads];
-      const testQuads = quads.map(quadItem => {
-        const { subject, predicate, object } = quadItem;
-        return quad(namedNode(subject), namedNode(predicate), object);
-      });
-      testQuads.forEach(quad => writer.addQuad(quad));
-      writer.end(async (error, result) => {
-        if (!error) {
-          await createACLFile(aclDocument, result);
-        }
-      });
-    } catch (e) {
-      throw e;
-    }
-  };
-
-  /**
    * Creates a new game based on an opponent's webId and a game document url with an acl file
    * @param {Event} e Submit event
    */
@@ -223,12 +124,16 @@ const GameForm = ({ webId, sendNotification, opponent, setOpponent }: Props) => 
         webId,
         `${process.env.REACT_APP_TICTAC_PATH}${documentUri}`
       );
-
       await createGame(documentPath, opponent);
-      await aclTurtle(documentPath, opponent);
+      const permissions = [
+        { agents: [opponent], modes: [AccessControlList.MODES.READ, AccessControlList.MODES.WRITE] }
+      ];
+      const ACLFile = new AccessControlList(webId, documentPath);
+      await ACLFile.createACL(permissions);
+      // await aclTurtle(documentPath, opponent);
       successToaster(t('game.createGameSuccess'), t('notifications.success'));
     } catch (e) {
-      errorToaster(e.message);
+      errorToaster(e.message, t('game.errorTitle'));
     }
   };
 
