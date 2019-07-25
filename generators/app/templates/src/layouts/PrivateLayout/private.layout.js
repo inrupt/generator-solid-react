64b418c8a0ca76cf9c6d5c504f0662d7fe4aa254
Jairo Campos <jairo.campos@janeirodigital.com>
2019-07-25 14:28:18 -0600

    added app permission message error

--- generators/app/templates/src/layouts/PrivateLayout/private.layout.js
+++ generators/app/templates/src/layouts/PrivateLayout/private.layout.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { withAuthorization } from '@inrupt/solid-react-components';
+import { useTranslation } from 'react-i18next';
+import { withAuthorization, AppPermission } from '@inrupt/solid-react-components';
 import { AuthNavBar, Footer } from '@components';
+import { errorToaster } from '@utils';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -18,35 +20,58 @@ const Content = styled.div`
   overflow-y: auto;
 `;
 
-const PrivateLayout = ({ routes, webId, location, history, ...rest }) => (
-  <React.Fragment>
-    <Container>
-      <Route
-        {...rest}
-        component={({ history }) => (
-          <Content className="contentApp">
-            <AuthNavBar {...{ location, webId, history }} />
-            <Switch>
-              {routes.map(route => {
-                const { component: RouteComponent } = route;
-                return (
-                  <Route
-                    key={route.id}
-                    path={route.path}
-                    render={routerProps => <RouteComponent {...routerProps} webId={webId} />}
-                    webId={webId}
-                    exact
-                  />
-                );
-              })}
-              <Redirect to="/404" />
-            </Switch>
-          </Content>
-        )}
-      />
-      <Footer />
-    </Container>
-  </React.Fragment>
-);
+const PrivateLayout = ({ routes, webId, location, history, ...rest }) => {
+  const { t } = useTranslation();
+  const checkAppPermissions = useCallback(async () => {
+    const permission = await AppPermission.checkPermissions(webId);
+    /**
+     * SDK app will need all the permission by the user pod,
+     * so for this reason we check it for that.
+     */
+    const permissions = permission.permissions.toString();
+
+    if (permissions !== process.env.REACT_APP_PERMISSIONS) {
+      errorToaster(t('appPermission.message'), 'Error', {
+        label: t('appPermission.link.label'),
+        href: t('appPermission.link.href')
+      });
+    }
+  }, [webId]);
+
+  useEffect(() => {
+    if (webId) checkAppPermissions();
+  }, [webId]);
+
+  return (
+    <React.Fragment>
+      <Container>
+        <Route
+          {...rest}
+          component={({ history }) => (
+            <Content className="contentApp">
+              <AuthNavBar {...{ location, webId, history }} />
+              <Switch>
+                {routes.map(route => {
+                  const { component: RouteComponent } = route;
+                  return (
+                    <Route
+                      key={route.id}
+                      path={route.path}
+                      render={routerProps => <RouteComponent {...routerProps} webId={webId} />}
+                      webId={webId}
+                      exact
+                    />
+                  );
+                })}
+                <Redirect to="/404" />
+              </Switch>
+            </Content>
+          )}
+        />
+        <Footer />
+      </Container>
+    </React.Fragment>
+  );
+};
 
 export default withAuthorization(PrivateLayout);
