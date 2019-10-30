7d39b0e307195b1e30a0d10a09b36d569897fb71
Michiel de Jong <michiel@unhosted.org>
2019-10-30 10:19:09 +0100

    Load media from @utils

--- generators/app/templates/src/containers/TextEditor/text-editor.style.js
+++ generators/app/templates/src/containers/TextEditor/text-editor.style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { media } from '../../utils';
+import { media } from '@utils';
 
 export const TextEditorWrapper = styled.section`
   display: flex;
