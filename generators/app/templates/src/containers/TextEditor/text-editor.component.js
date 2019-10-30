04469d20df2589dbfe84dfbe767e7153a21ccf9a
Michiel de Jong <michiel@unhosted.org>
2019-10-30 10:15:48 +0100

    Apply rest of review comments

--- generators/app/templates/src/containers/TextEditor/text-editor.component.js
+++ generators/app/templates/src/containers/TextEditor/text-editor.component.js
@@ -1,12 +1,13 @@
 /* eslint-disable constructor-super */
 import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { TextEditorWrapper, TextEditorContainer, Header, Form, FullGridSize, Button, Label, Input, TextArea, WebId } from './text-editor.style';
 import SolidAuth from 'solid-auth-client';
 import { successToaster, errorToaster } from '@utils';
 import LinkHeader from 'http-link-header';
 // import ldflex from '@solid/query-ldflex';
 import { fetchDocument, createDocument } from 'tripledoc';
-import { rdf, acl /*, pim */ } from 'rdf-namespaces';
+import { AccessControlList } from '@inrupt/solid-react-components';
 import { useWebId } from '@solid/react';
 
 const pim = { storage: 'http://www.w3.org/ns/pim/space#storage' };
@@ -23,7 +24,6 @@ function extractAclRef(response, documentRef) {
       aclRef = new URL(aclLinks[0].uri, documentRef).href;
     }
   }
-  console.log('aclRef', aclRef);
   if (!aclRef) {
     aclRef = documentRef + '.acl';
   }
@@ -48,7 +48,6 @@ function extractWacAllow(response) {
   };
   const wacAllowHeader = response.headers.get('WAC-Allow');
   if (wacAllowHeader) {
-    console.log({ wacAllowHeader });
     wacAllowHeader // 'user="read write append control",public="read"'
       .split(',') // ['user="read write append control"', 'public="read"']
       .map(str => str.trim())
@@ -56,7 +55,6 @@ function extractWacAllow(response) {
          const parts = statement.split('='); // ['user', '"read write control"']
          if (parts.length >= 2 && ['user', 'public'].indexOf(parts[0]) !== -1 && parts[1].length > 2) {
            const modeStr = parts[1].replace(/"/g, ''); // 'read write control' or ''
-           console.log(`modes for ${parts[0]}`, parts, modeStr);
            if (modeStr.length) {
             modeStr.split(' ').forEach(mode => {
               modes[parts[0]][mode] = true; 
@@ -65,11 +63,11 @@ function extractWacAllow(response) {
         }
       });
   }
-  console.log({ modes });
   return modes;
 }
 
 export const Editor = (props) => {
+  const { t } = useTranslation();
   const [url, setUrl] = useState('');
   const [aclUrl, setAclUrl] = useState('');
   const [friend, setFriend] = useState('https://example-friend.com/profile/card#me');
@@ -117,57 +115,47 @@ export const Editor = (props) => {
 
   function handleLoad(event) {
     event.preventDefault();
-    console.log('handle load!');
     const doc = SolidAuth.fetch(url);
     doc.then(async (response) => {
       const text = await response.text();
-      console.log('fetched', text);
       if (response.ok) {
         setText(text);
+      } else if (response.status === 404) {
+        successToaster(t('notifications.404'));
+      } else {
+        errorToaster(t('notifications.errorLoading'));
       }
       const wacAllowModes = extractWacAllow(response);
       setEditable(wacAllowModes.user.write);
       setSharable(wacAllowModes.user.control);
       setAclUrl(extractAclRef(response, url));
       setLoaded(true);
+    }).catch((e) => {
+      // console.error(e.message, e);
+      errorToaster(t('notifications.errorFetching'));
     });
   } // assuming the logged in user doesn't change without a page refresh
 
   async function handleShare(event) {
     event.preventDefault();
     try {
-      // not sure how to do this with ldflex,
-      // will use TripleDoc for now:
-      let doc;
-      try {
-        doc = await fetchDocument(aclUrl);
-      } catch(e) {
-        doc = createDocument(aclUrl);
-      }
-
-      const subOwner = doc.getSubject(new URL('#owner', url).toString());
-      subOwner.setNodeRef(rdf.type, acl.Authorization);
-      subOwner.setNodeRef(acl.agent, webId);
-      subOwner.setNodeRef(acl.accessTo, url);
-      subOwner.addNodeRef(acl.mode, acl.Read);
-      subOwner.addNodeRef(acl.mode, acl.Write);
-      subOwner.addNodeRef(acl.mode, acl.Control);
-      const subSharedWith = doc.addSubject();
-      subSharedWith.setNodeRef(rdf.type, acl.Authorization);
-      subSharedWith.setNodeRef(acl.agent, friend);
-      subSharedWith.setNodeRef(acl.accessTo, url);
-      subSharedWith.addNodeRef(acl.mode, acl.Read);
-      subSharedWith.addNodeRef(acl.mode, acl.Write);
-      await doc.save();
-      console.log('ACL saved');
-      successToaster(`${friend} now has read/write access.`);
+      const permissions = [
+        {
+          agents: [friend],
+          modes: [AccessControlList.MODES.READ, AccessControlList.MODES.WRITE]
+        }
+      ];
+      const ACLFile = new AccessControlList(webId, url);
+      await ACLFile.createACL(permissions);
+      successToaster(t('notifications.accessGranted'));
     } catch (e) {
-      errorToaster('There was an error sharing your file, please try again.');
+      errorToaster(t('notifications.errorGrantingAccess'));
     }
   }
 
   async function handleSave(event) {
     event.preventDefault();
+    // Not using TripleDoc or LDFlex here, because this is not an RDF document.
     const result = await SolidAuth.fetch(url, {
       method: 'PUT',
       body: text,
@@ -177,7 +165,7 @@ export const Editor = (props) => {
     });
 
     if (result.ok) {
-      successToaster('Your file was saved successfully.');
+      successToaster(t('notifications.saved'));
     } else if(result.ok === false) {
       errorToaster('There was an error saving your file, please try again.');
     }
@@ -197,7 +185,7 @@ export const Editor = (props) => {
           <Button className="ids-link-filled ids-link-filled--primary button" onClick={handleLoad}>Load</Button>
           {editable ?
             <Button className="ids-link-filled ids-link-filled--secondary button" onClick={handleSave}>Save</Button>
-          : (loaded ? '(not editable)' : '')}
+          : (loaded ? t('notifications.notEditable') : '')}
         </div>
       </FullGridSize>
       <FullGridSize>
@@ -208,8 +196,8 @@ export const Editor = (props) => {
           Share:
           <Input type="text" size="200" value={friend} onChange={handleFriendChange} />
         </Label>
-        <Button className="ids-link-stroke ids-link-stroke--primary button" onClick={handleShare}>Share</Button>
-      </FullGridSize>: '(log in and load a doc on your own pod to enable sharing'}
+        <Button className="ids-link-stroke ids-link-stroke--primary button" onClick={handleShare}>Grant Access</Button>
+      </FullGridSize>: t('notifications.notSharable')}
     </Form>
   );
 }
@@ -224,11 +212,7 @@ const TextEditor = () => {
       <TextEditorContainer>
         <Header>
           <p>
-          This simple text editor enables you to create a plaintext file and save it in a Pod.
-          To make this very clear for the demo you provide a URL as an absolute path to the file in the Pod.
-          The Pod can be the one this App has been logged into or another person's Pod.
-          To write, and read, into another person's Pod the owner will have had to provide you with
-          write permission.
+          t('editor.explanation')
           </p>
         </Header>
         <Editor/>
